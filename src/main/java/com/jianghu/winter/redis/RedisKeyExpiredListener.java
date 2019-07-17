dc22bc3aa4cd6019f1ff65dc684b82ffcccd7c0b
huguanglei <guanglei.hu@baozun.com>
2019-07-17 16:33:19 +0800

    redis 监听key过期事件

---
+++ src/main/java/com/jianghu/winter/redis/RedisKeyExpiredListener.java
@@ -0,0 +1,27 @@
+package com.jianghu.winter.redis;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author daniel.hu
+ * @date 2019/7/17 13:37
+ */
+@Slf4j
+public class RedisKeyExpiredListener implements MessageListener {
+
+    /**
+     * 监听订阅的topic，针对redis数据失效事件，进行数据处理
+     */
+    @Override
+    public void onMessage(Message message, byte[] bytes) {
+        String channel = new String(message.getChannel(), StandardCharsets.UTF_8);
+        // 过期的key(java中不可能得到这个redis-key对应的redis-value)
+        String key = new String(message.getBody(), StandardCharsets.UTF_8);
+        log.info("redis key 过期：pattern为{}, channel为{}, key为{}", new String(bytes), channel, key);
+    }
+
+}
---
+++ src/main/java/com/jianghu/winter/redis/RedisListenerConfig.java
@@ -0,0 +1,28 @@
+package com.jianghu.winter.redis;
+
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+/**
+ * @author daniel.hu
+ * @date 2019/7/17 13:35
+ */
+@Configuration
+@AutoConfigureAfter(RedisAutoConfiguration.class)
+public class RedisListenerConfig {
+
+    private static final String KEY_EVENT_0_EXPIRED = "__keyevent@0__:expired";
+
+    @Bean
+    RedisMessageListenerContainer container(LettuceConnectionFactory lettuceConnectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(lettuceConnectionFactory);
+        container.addMessageListener(new RedisKeyExpiredListener(), new PatternTopic(KEY_EVENT_0_EXPIRED));
+        return container;
+    }
+}
