55c93f365502280f2996a0009455bf981d4ba007
huguanglei <guanglei.hu@baozun.com>
2019-08-01 13:58:14 +0800

    feign local 走gateway

---
+++ src/main/java/com/jianghu/winter/remote/GatewayRequestInterceptor.java
@@ -0,0 +1,33 @@
+package com.jianghu.winter.remote;
+
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * GatewayRequestInterceptor
+ *
+ * @author daniel.hu
+ */
+@Slf4j
+@Component
+public class GatewayRequestInterceptor implements RequestInterceptor {
+
+    @Setter
+    @Value(RemoteConstant.GATEWAY_URL)
+    private String gatewayUrl;
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        if (StringUtils.startsWith(gatewayUrl, "http://gateway")) {
+            requestTemplate.header(RemoteConstant.CLIENT_ID, RemoteConstant.CLIENT_ID_VAL);
+            requestTemplate.header(RemoteConstant.CLIENT_SECRET, RemoteConstant.CLIENT_SECRET_VAL);
+        }
+    }
+
+}
---
+++ src/main/java/com/jianghu/winter/remote/RemoteConstant.java
@@ -0,0 +1,23 @@
+package com.jianghu.winter.remote;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * RemoteConstant
+ *
+ * @author daniel.hu
+ * @date 2019-08-03
+ */
+@SuppressWarnings("squid:S1118")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class RemoteConstant {
+    public static final String GATEWAY_URL = "${winter.gateway.url:}";
+
+    public static final String CLIENT_ID = "client-id";
+    public static final String CLIENT_ID_VAL = "client-id-value";
+
+    public static final String CLIENT_SECRET = "client-secret";
+    public static final String CLIENT_SECRET_VAL = "client-secret-value";
+
+}
