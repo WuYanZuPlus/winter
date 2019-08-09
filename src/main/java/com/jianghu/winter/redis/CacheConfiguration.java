f6ccd8c8a4c8d35f304790c69a05511cb28ce3e8
huguanglei <guanglei.hu@baozun.com>
2019-08-09 10:17:50 +0800

    忽略spring-cache的超时异常

--- src/main/java/com/jianghu/winter/redis/CacheConfiguration.java
+++ src/main/java/com/jianghu/winter/redis/CacheConfiguration.java
@@ -3,9 +3,13 @@ package com.jianghu.winter.redis;
 import com.alibaba.fastjson.parser.ParserConfig;
 import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
 import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.cache.interceptor.SimpleCacheErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -13,7 +17,9 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.*;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
 import java.util.HashMap;
@@ -23,6 +29,7 @@ import java.util.Map;
  * @author daniel.hu
  * @date 2019/08/07
  */
+@Slf4j
 @Configuration
 @EnableCaching(proxyTargetClass = true)
 public class CacheConfiguration {
@@ -53,6 +60,21 @@ public class CacheConfiguration {
         redisCacheConfigurationMap.put(CacheConstant.TEST_TOKEN, getRedisCacheConfiguration(60L));
 
         return redisCacheConfigurationMap;
+    }
+
+    @Bean
+    public CacheErrorHandler cacheErrorHandler() {
+        return new SimpleCacheErrorHandler() {
+            @Override
+            public void handleCacheGetError(RuntimeException e, Cache cache, Object key) {
+                log.warn("缓存不可用: {}, cache={}, key={}", e.getMessage(), cache.getName(), key);
+            }
+
+            @Override
+            public void handleCachePutError(RuntimeException e, Cache cache, Object key, Object value) {
+                log.warn("缓存不可用: {}, cache={}, key={}", e.getMessage(), cache.getName(), key);
+            }
+        };
     }
 
     @Deprecated
